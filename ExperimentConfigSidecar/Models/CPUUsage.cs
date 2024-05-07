142df587e32f5a91f2c00f29772d802fb18ba2a9
nk-coding <niklask.coding@gmail.com>
2024-05-07 15:43:07 +0200

    improve docs

--- ExperimentConfigSidecar/Models/CPUUsage.cs
+++ ExperimentConfigSidecar/Models/CPUUsage.cs
@@ -2,6 +2,14 @@ namespace ExperimentConfigSidecar.Models;
 
 /// <summary>
 /// Represents the CPU usage configuration.
+/// A CPU usage works like this:
+/// <code>
+/// while (true)
+/// {
+///     cycle the cpu for UsageDuration milliseconds
+///     pause for PauseDuration milliseconds
+/// }
+/// </code>
 /// </summary>
 /// <param name="UsageDuration">The duration in milliseconds to use CPU (keep a thread spinning).</param>
 /// <param name="PauseDuration">The duration in milliseconds to pause between CPU usage.</param>
